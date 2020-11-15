46146211efda2781105a2ca2a0b80e8d202f8054
Esteban Victorio <estebanvictorio92@gmail.com>
2020-11-14 20:56:52 -0800

    feature: add testing and next pages in the book

--- src/lib.rs
+++ src/lib.rs
@@ -10,20 +10,19 @@ pub fn print(limit: u8) {
 }
 
 
-// NOTE: Last checkpoint: change signature argument input from Vec to reference of a slice
 fn output_sequence(numbers: &[Number]) {
   for n in numbers {
     println!("{}", n);
   }
 }
  
-fn generate_sequence(limit: u8)-> Vec<Number> {
+pub fn generate_sequence(limit: u8)-> Vec<Number> {
   (1..=limit)
     .map(|value| Number { name: String::from("auto"), value, })
     .collect()
 }
 
-struct Number {
+pub struct Number {
   name: String,
   value: u8,
 }
@@ -34,4 +33,15 @@ impl fmt::Display for Number {
   fn fmt(&self, f:&mut fmt::Formatter<'_>) -> fmt::Result {
     write!(f, "(Hi! Im a Number, my name is {}, and my value is  {})", self.name, self.value)
   }
+}
+
+// NOTE: last page: 46 - testing and wrapping up
+// NOTE: next page: 47 - Actix Web app
+
+#[test]
+fn it_should_generate_at_least_auto_number_one() {
+  let list = generate_sequence(1);
+  let one = &list[0];
+  assert_eq!(String::from("auto"), one.name);
+  assert_eq!(1,one.value);
 }
